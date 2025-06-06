514a48583c5238bd97973dd6d1358cd2879af175
rslehtola <rslehtola@students.everettcc.edu>
2025-06-06 14:57:20 +0000

    debugging welcome and greetings. Started CSV loading and error messages in main

--- main.py
+++ main.py
@@ -3,35 +3,42 @@
 # Date: 06/02/25
 # Class: STEM 103 Spring
 
-import random
-import csv
-
 
 # Welcome the user and prompt them for their name also store it as a variable
 def welcome_user():
-    user_name=input("Welcome to the Misinformation Station Trivia! What's your name?")
+    user_name=input("Welcome to the Misinformation Station Trivia! What's your name? ")
     print(f"Greetings {user_name}! Let's get started!") # Greet the user by their name
     return user_name
+
+welcome_user() 
+
+import random
+import csv
 
 # Load questions from CSV
 def load_questions(file_path):
     try:
         with open(file_path, mode='r') as file:
-            reader=csv.reader(file)
-            data=list(reader)
-            header=data[0]
-            questions=data[1:]
-            random.shuffle(questions)
-            return questions
+            reader = csv.reader(file)
+            data = list(reader)
+           
+        if not data: # file not found display for CSV
+            print("File not found")
+            return None
+    
+        header = data[0] # Loading from CSV
+        questions = data[1:]
+        
+        if not questions:
+            print("No questions found") # Questions not found display
+            return None
+
+        random.shuffle(questions) # Implement random question shuffle
+        return questions
+    
     except FileNotFoundError:
-        print(f"Error!!! Could not find file '{file_path}'")
+        print(f"Error!!! Could not find file '{file_path}'") # File not found error display
         return None
-
-# Impement random question shuffle
-
-
-# File not found error display
-
 
 # Prompt the user to answer a question
 
