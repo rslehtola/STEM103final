846422efcc228bc07b35389f669f043fdba9d0c6
rslehtola <rslehtola@students.everettcc.edu>
2025-06-06 02:02:30 +0000

    Pseudo code input into main

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 # Project: Misinformation Station Trivia
 # Coder: Rory Lehtola
 # Date: 06/02/25
+
+
+# Welcome the user and prompt them for their name also store it as a variable
+# Greet the user by their name
+# Load questions from CSV
+# Impement random question shuffle
+# File not found error display
+# Prompt the user to answer a question
+# Prompt the user to input a number between 1-4
+# Input user answer
+# Check user answer is 1-4
+# Error display on bad answer and prompt please pick an answer between 1-4 
+# If users answer was correct display
+# If user answer was incorrect display correct answer
+# Keep track of the score
+# Press enter to contiue prompt
+# Display users name with the final score
