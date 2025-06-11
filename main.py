7070e30508522dc6d81d6d36bb49395e0e034c3a
rslehtola <rslehtola@students.everettcc.edu>
2025-06-11 00:58:07 +0000

    inputed program notes into main

--- main.py
+++ main.py
@@ -3,6 +3,22 @@
 # Date: 06/02/25
 # Class: STEM 103 Spring
 
+# Program:
+# Within this program is a beautiful trivia game. The questions for this game are completely 
+# based on stupid comments and statemnets made by Donald Trump from which are inaccurate or untrue. 
+# However, these questions and answers are true. Once ran, this program will welcome the user to 
+# the game and prompt them to input their name. Once they input their name and hit enter it will 
+# greet them by name and ask them to play a game and load the first question of twenty-four with 
+# a multiple choice answer. The user only needs to input a number between 1-4 and hit enter. If 
+# the user has answered the question incorrectly, it will display such and tell them what the 
+# correct answer was and prompt them to hit enter to continue to the next question. If the user 
+# answers the question correctly it will display a congradulation and prompt them to hit enter 
+# when they're ready to continue to the next question. If the user doesn't answer with a number 
+# between 1-4 it'll dispaly that it's not a valid number and ask them to please enter a 1, 2, 3, 
+# or 4. Once all twenty-four questions are answered it displays your final score and shows good 
+# sportsmanship with a good game and displaying the users name. I've attempted to answer my 
+# questions with every symbol, character, letter, number and it won't accept any answer other 
+# than 1, 2, 3 or 4.
 
 # Function 1: Welcome the user and prompt them for their name also store it as a variable
 def welcome_user():
@@ -25,7 +41,6 @@ def load_questions(file_path):
             print("Error!!! File is empty") # Inform user of empty file
             return None # Return none so the game doesn't continue
 
-        header = data[0] # Store the header row
         questions = data[1:] # Store all the rows except the header as question data
         
         if not questions: # If there as no actual questions
